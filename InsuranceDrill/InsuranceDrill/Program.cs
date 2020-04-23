77009848d8d14e473d1c000f220868f50cc9f37c
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-04-23 13:36:51 -0500

    Insurance Approval Program Drill
    Drill for approving insurance based on the answers to questions

---
+++ InsuranceDrill/InsuranceDrill/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InsuranceDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insurance Approval Program");
+
+            Console.WriteLine("What is  your age?");
+            int personAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Please answer 'true' or 'false'.");
+            bool personDui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int personTicket = Convert.ToInt32(Console.ReadLine());
+            bool doesQualify = ((personAge > 15) && (personDui == false) && (personTicket < 3));
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(doesQualify);
+        }
+    }
+}
