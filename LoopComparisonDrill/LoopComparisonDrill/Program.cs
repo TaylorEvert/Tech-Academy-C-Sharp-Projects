503005edfaf1d632896ec66ec59e87fa5e10bfb2
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-04-23 15:10:12 -0500

    Loop Comparison Drill
    Using while loops and do while loops

---
+++ LoopComparisonDrill/LoopComparisonDrill/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LoopComparisonDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number between 1 and 10");
+            int numberChosen = Convert.ToInt32(Console.ReadLine());
+            bool isOver = false;
+
+            while (numberChosen <= 10)
+            {
+                Console.WriteLine(numberChosen);
+                numberChosen += 1;
+                if (numberChosen > 10)
+                {
+                    isOver = true;
+                }
+            }
+
+            do
+            {
+                Console.WriteLine(numberChosen);
+                numberChosen--;
+                if (numberChosen < 1)
+                {
+                    isOver = false;
+                    Console.WriteLine("Done counting!");
+                }
+            } while (isOver);
+        }
+    }
+}
