aab0baa208b6af4789dc96667a645f1fda32928c
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-04-30 14:52:13 -0500

    Try/Catch with Integers

---
+++ TryCatchIntegers/TryCatchIntegers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TryCatchIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> numberList = new List<int>() { 800, 250, 325, 999, 225 };
+                Console.WriteLine("Please enter a number. This number will be used to divide the numbers in the list.");
+                int divideNumber = Convert.ToInt32(Console.ReadLine());
+                foreach (int number in numberList)
+                {
+                    int total = number / divideNumber;
+                    Console.WriteLine("Output: " + total);
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Cannot divide by zero.");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please enter a whole number. ");
+            }
+            finally
+            {
+                Console.WriteLine("Program has emerged from the try/catch block. ");
+            }
+        }
+    }
+}
