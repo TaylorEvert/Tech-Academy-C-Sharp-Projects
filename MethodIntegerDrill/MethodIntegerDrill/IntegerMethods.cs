435b82099cf46fe7bcbf21b715bbc6a88900a7d0
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-05-06 13:46:49 -0500

    Method Integer Drill
    - Create Methods in a separate file class and return value to the console after calculating user input.

---
+++ MethodIntegerDrill/MethodIntegerDrill/IntegerMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodIntegerDrill
+{
+    class IntegerMethods
+    {
+        public static int IntegerOne(int methodOne)
+        {
+            int outputOne = (methodOne * 100) / 5;
+            return outputOne;
+        }
+
+        public static int IntegerTwo(int methodTwo)
+        {
+            int outputTwo = (methodTwo + methodTwo) / methodTwo;
+            return outputTwo;
+        }
+
+        public static int IntegerThree(int methodThree)
+        {
+            int outputThree = (methodThree * 20) + (methodThree - methodThree);
+            return methodThree;
+        }
+    }
+}
---
+++ MethodIntegerDrill/MethodIntegerDrill/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MethodIntegerDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // First method to do work with
+            Console.WriteLine("Please enter a whole number: ");
+            int methodOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Method One Output: " + IntegerMethods.IntegerOne(methodOne));
+
+            // Second method input for math
+            Console.WriteLine("Please enter another whole number: ");
+            int methodTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Method Two Output: " + IntegerMethods.IntegerTwo(methodTwo));
+
+            // Third method to produce outpute based on user input
+            Console.WriteLine("Please enter the final whole number: ");
+            int methodThree = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Method Three Output: " + IntegerMethods.IntegerThree(methodThree));
+        }
+    }
+}
