a3ee52c5286e96a1e5472dd7bbe9dbc510309f74
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-05-06 15:26:17 -0500

    Class Overload Drill

---
+++ ClassOverloadDrill/ClassOverloadDrill/DrillMethods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassOverloadDrill
+{
+    class DrillMethods
+    {
+
+        public static int MethodOne(int numberOne )
+        {
+            int result = (numberOne * 15) + 20;
+            return result;
+        }
+
+        public static double MethodOne(int numberOne, double decimalOne)
+        {
+            double decimalTwo = Convert.ToDouble(numberOne);
+            double result = (decimalOne + decimalTwo) * 10;
+            return result;
+        }
+
+        public static double MethodOne(int numberOne, double decimalOne, string stringOne)
+        {
+            int stringResult = Convert.ToInt32(stringOne);
+            double stringDouble = Convert.ToDouble(stringResult);
+            double integerDouble = Convert.ToDouble(numberOne);
+            double result = (stringDouble + decimalOne + integerDouble) * 100;
+            return result;
+
+
+        }
+    }
+}
---
+++ ClassOverloadDrill/ClassOverloadDrill/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassOverloadDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numberOne = DrillMethods.MethodOne(50);
+            Console.WriteLine(numberOne);
+
+            double numberTwo = DrillMethods.MethodOne(50, 56.7);
+            Console.WriteLine(numberTwo);
+
+            double stringNumber = DrillMethods.MethodOne(50, 56.7, "12");
+            Console.WriteLine(stringNumber);
+        }
+    }
+}
