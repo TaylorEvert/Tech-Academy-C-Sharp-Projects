378ac3c55a06de283dfd8f60691886a9efe0494b
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-05-07 15:15:07 -0500

    Overload Void Drill

---
+++ OverloadVoidDrill/OverloadVoidDrill/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OverloadVoidDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidMethod methodOne = new VoidMethod();
+            Console.WriteLine("Please enter a whole number: ");
+            int answerOne = Convert.ToInt32(Console.ReadLine());
+            VoidMethod.NumberMethod(out int resultMain, answerOne);
+            Console.WriteLine(resultMain);
+
+
+        }
+    }
+}
---
+++ OverloadVoidDrill/OverloadVoidDrill/VoidMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OverloadVoidDrill
+{
+    class VoidMethod
+    {
+        public static void NumberMethod(out int resultOne, int numberOne)
+        {
+            resultOne = numberOne / 2;
+            
+        }
+    }
+}
