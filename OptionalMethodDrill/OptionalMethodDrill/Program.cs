85c8f059769de545a1c2c780e400f6c5f26326a0
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-05-07 15:59:18 -0500

    Temporary Optional Drill

---
+++ OptionalMethodDrill/OptionalMethodDrill/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OptionalMethodDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole number: ");
+            int answerOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another whole number. This is optional: ");
+            int results;
+            string answerTwo = Console.ReadLine();
+            if (answerTwo == null)
+            {
+                results = Methods.OptionalMethod(answerOne);
+            }
+            else 
+            {
+                int answerThree = Convert.ToInt32(answerTwo);
+                results = Methods.OptionalMethod(answerOne, answerThree);
+            }
+
+
+            Console.WriteLine("Results: " + results);
+        }
+    }
+}
