4b48134ee2f65ed67fabfd67b156f5e0dc09255f
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-05-07 13:38:34 -0500

    Optional Methods Drill

---
+++ OptionalMethodDrill/OptionalMethodDrill/Methods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OptionalMethodDrill
+{
+    class Methods
+    {
+        public static int OptionalMethod(int numberOne, int numberTwo = 0)
+        {
+            int mathOne = (numberOne * 10) / 5;
+            int mathTwo = (numberTwo * 5) / 2;
+            int result = (mathOne + mathTwo);
+            return result;
+        }
+    }
+}
---
+++ OptionalMethodDrill/OptionalMethodDrill/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OptionalMethodDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole number: ");
+            int answerOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another whole number. This is optional, please enter '0' if you wish to not answer: ");
+            int answerTwo = Convert.ToInt32(Console.ReadLine());
+
+            int results = Methods.OptionalMethod(answerOne, answerTwo);
+            Console.WriteLine("Results: " + results);
+        }
+    }
+}
