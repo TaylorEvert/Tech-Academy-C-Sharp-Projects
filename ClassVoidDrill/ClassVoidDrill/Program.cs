c30cf3dbf04e1449c5934fac4b96ede29156f403
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-05-07 14:09:42 -0500

    Class Void Drill

---
+++ ClassVoidDrill/ClassVoidDrill/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassVoidDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidMethod.MathMethod(10, 10);
+            VoidMethod.MathMethod(numberOne: 15, numberTwo: 15);
+
+        }
+    }
+}
---
+++ ClassVoidDrill/ClassVoidDrill/VoidMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassVoidDrill
+{
+    class VoidMethod
+    {
+        public static void MathMethod(int numberOne, int numberTwo)
+        {
+            int mathOne = numberOne * numberOne;
+            Console.WriteLine(numberTwo);
+            Console.WriteLine(mathOne);
+        }
+    }
+}
