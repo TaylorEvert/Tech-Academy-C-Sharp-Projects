2ea9a19462d340a326a0dbacb9efc89a7fdc5277
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-05-11 13:33:07 -0500

    Superclass Drill

---
+++ SuperClassDrill/SuperClassDrill/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SuperClassDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Employee employee = new Employee();
+
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+
+            employee.SayName();
+        }
+    }
+}
