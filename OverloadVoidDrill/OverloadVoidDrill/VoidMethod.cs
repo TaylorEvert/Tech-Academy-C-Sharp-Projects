181e153b8d11a62fd02522c573da4a8bd935308a
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-05-11 12:55:20 -0500

    Overload Void Drill - Complete

--- OverloadVoidDrill/OverloadVoidDrill/VoidMethod.cs
+++ OverloadVoidDrill/OverloadVoidDrill/VoidMethod.cs
@@ -11,5 +11,9 @@ namespace OverloadVoidDrill
             resultOne = numberOne / 2;
             
         }
+        public static void NumberMethod(out int resultOne, int numberOne, int numberTwo = 0)
+        {
+            resultOne = (numberOne / 2) * numberTwo;
+        }
     }
 }
