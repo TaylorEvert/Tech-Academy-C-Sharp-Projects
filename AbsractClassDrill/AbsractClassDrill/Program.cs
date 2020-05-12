f5f7ff96c932d9e62d5bb15d270bdb16f75f3c3e
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-05-12 14:57:48 -0500

    Abstract Classes and Methods Drill

---
+++ AbsractClassDrill/AbsractClassDrill/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AbsractClassDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            Employee.FirstName = "Sample";
+            Employee.LastName = "Student";
+
+            employee.SayName();
+        }
+    }
+}
