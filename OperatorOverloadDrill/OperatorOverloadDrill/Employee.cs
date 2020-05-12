20bc3e3b9daa81652b8d453c2381328f03f7336e
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-05-12 16:28:25 -0500

    Operator Overload Drill

---
+++ OperatorOverloadDrill/OperatorOverloadDrill/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorOverloadDrill
+{
+    class Employee: Person, IQuittable
+    {
+        public int Id { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("This line was generated from a method that was inherited from an interface. ");
+        }
+
+        public static bool operator ==(Employee X, Employee Y)
+        {
+            bool status = false;
+            if (X == Y)
+            {
+                status = true;
+            }
+            return status;
+        }
+
+        public static bool operator !=(Employee X, Employee Y)
+        {
+            bool status = true;
+            if (X != Y)
+            {
+                status = false;
+            }
+            return status;
+        }
+}
---
+++ OperatorOverloadDrill/OperatorOverloadDrill/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OperatorOverloadDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable quittable = new Employee();
+            quittable.Quit();
+        }
+    }
+}
