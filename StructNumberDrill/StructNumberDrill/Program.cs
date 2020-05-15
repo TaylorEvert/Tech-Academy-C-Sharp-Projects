00f0ec2f4fb2dd461333ef6ee9a2cace35a923df
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-05-15 14:01:26 -0500

    Struct Number Drill

---
+++ StructNumberDrill/StructNumberDrill/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StructNumberDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 15.5M;
+            Console.WriteLine(number.Amount);
+        }
+    }
+}
