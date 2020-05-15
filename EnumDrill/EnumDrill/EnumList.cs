f4d3a7980998d300ecf5c3e42c78b359580e0a1a
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-05-15 13:36:09 -0500

    Enumeration Days of The Week Drill

---
+++ EnumDrill/EnumDrill/EnumList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnumDrill
+{
+    public class EnumList
+    {
+
+    }
+    public enum DaysOfTheWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ EnumDrill/EnumDrill/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnumDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week: ");
+                string dayAnswer = Console.ReadLine();
+
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), dayAnswer);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+        }
+    }
+}
