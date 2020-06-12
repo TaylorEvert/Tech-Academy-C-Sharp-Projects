5707c00d720c7ac22bfa6428b7de6c7240690bb1
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-06-12 13:20:11 -0500

    Number to File Drill

---
+++ NumberTextFile/NumberTextFile/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace NumberTextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Please enter a number: ");
+            string answer = Console.ReadLine();
+
+            File.WriteAllText(@"logdrill1.txt", answer);
+            Console.WriteLine("File Says: " + File.ReadAllText(@"logdrill1.txt"));
+        }
+    }
+}
