6d867f0802c5f31585872716d0769a864c09f501
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-06-12 14:41:01 -0500

    Add number to date drill

---
+++ DateNumberDrill/DateNumberDrill/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DateNumberDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Hello! Please enter a number: ");
+            double answer = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(DateTime.Now.AddHours(answer));
+        }
+    }
+}
