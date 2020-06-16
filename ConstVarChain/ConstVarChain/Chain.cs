0713bad671defe3a3728c332901316ef79d07dbf
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-06-16 15:34:47 -0500

    Constants Variables Constructors

---
+++ ConstVarChain/ConstVarChain/Chain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstVarChain
+{
+    public class Chain
+    {
+        public Chain(string namedPerson) : this(namedPerson, namedLocation: "Earth") { }
+        public Chain(string namedPerson, string namedLocation)
+        {
+            string name = namedPerson;
+            string location = namedLocation;
+            Console.WriteLine("Thank you {0}, from {1} for using my program! Have a great day!", name, location);
+        }
+
+    }
+}
---
+++ ConstVarChain/ConstVarChain/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConstVarChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string welcome = "Welcome to my program! ";
+            Console.WriteLine(welcome + "What is your name?");
+            var personName = Console.ReadLine();
+            Console.WriteLine("Where are you from? You do not need to answer this question if you feel uncomfortable doing so! ");
+            var personLocation = Console.ReadLine();
+            if (personLocation == "")
+            {
+                Chain chain1 = new Chain(personName);
+            }
+            else
+            {
+                Chain chain = new Chain(personName, personLocation);
+            }
+
+        }
+    }
+}
