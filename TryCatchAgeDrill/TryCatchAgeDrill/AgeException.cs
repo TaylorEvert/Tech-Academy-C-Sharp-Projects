86fd5c111a04d84ed4630d8b60a636b70af360f3
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-06-17 13:22:27 -0500

    Try & Catch with Age Drill

---
+++ TryCatchAgeDrill/TryCatchAgeDrill/AgeException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TryCatchAgeDrill
+{
+    public class AgeException : Exception
+    {
+        public AgeException()
+            : base() { }
+
+        public AgeException(string message)
+            : base(message) { }
+    }
+}
---
+++ TryCatchAgeDrill/TryCatchAgeDrill/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TryCatchAgeDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! What is your age?");
+            try
+            {
+                int personAge = Convert.ToInt32(Console.ReadLine());
+                if (personAge <= 0)
+                {
+                    throw new AgeException();
+                }
+                var today = DateTime.Today;
+                var birthYear = today.AddYears(-personAge);
+                Console.WriteLine(birthYear);
+
+            }
+            catch(AgeException)
+            {
+                Console.WriteLine("Please enter a valid age. This cannot be less than or equal to zero. Thank You.");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("An error as occured. Please contact support, thank you.");
+            }
+        }
+    }
+}
